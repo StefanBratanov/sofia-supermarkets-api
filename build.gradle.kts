30d974ab3239806d1f65cdb33b40238ccab5ab09
Stefataa <stefan.bratanov@yahoo.com>
2021-03-19 23:27:26 +0000

    using lettuce instead of jedis

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("redis.clients:jedis:3.5.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
