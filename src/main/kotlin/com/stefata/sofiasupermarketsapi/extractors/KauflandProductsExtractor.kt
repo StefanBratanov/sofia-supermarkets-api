ab1dc7ce705a659446bcfcfa335bfc0d86412a9b
Stefataa <stefan.bratanov@yahoo.com>
2021-04-01 11:20:57 +0100

    using the large image when retrieving for kaufland

--- src/main/kotlin/com/stefata/sofiasupermarketsapi/extractors/KauflandProductsExtractor.kt
+++ src/main/kotlin/com/stefata/sofiasupermarketsapi/extractors/KauflandProductsExtractor.kt
@@ -7,13 +7,17 @@ import com.stefata.sofiasupermarketsapi.common.normalizePrice
 import com.stefata.sofiasupermarketsapi.interfaces.UrlProductsExtractor
 import com.stefata.sofiasupermarketsapi.model.Product
 import org.apache.commons.lang3.StringUtils
+import org.apache.commons.validator.routines.UrlValidator
 import org.springframework.stereotype.Component
 import java.net.URL
+import java.util.*
 import kotlin.text.RegexOption.IGNORE_CASE
 
 @Log
 @Component("Kaufland")
-class KauflandProductsExtractor : UrlProductsExtractor {
+class KauflandProductsExtractor(
+    private val urlValidator: UrlValidator = UrlValidator()
+) : UrlProductsExtractor {
 
     override fun extract(url: URL): List<Product> {
 
@@ -33,7 +37,15 @@ class KauflandProductsExtractor : UrlProductsExtractor {
             }
             val price = it.select(".a-pricetag__price")?.text()
 
-            val picUrl = it.select(".m-offer-tile__image img")?.attr("data-src")
+            var picUrl = it.select(".m-offer-tile__image img")?.attr("data-srcset")
+                ?.split(",")?.map { urls -> urls.trim() }?.lastOrNull()
+                ?.split("\\s+".toRegex())?.first { picUrl ->
+                    urlValidator.isValid(picUrl)
+                }
+
+            if (Objects.isNull(picUrl)) {
+                picUrl = it.select(".m-offer-tile__image img")?.attr("data-src")
+            }
 
             Product(
                 name = StringUtils.normalizeSpace("$subtitle $title"),
