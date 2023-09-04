39fb098bbd6f1ee5f012ed44380cb29b96cb5724
Stefan Bratanov <stefan.bratanov93@gmail.com>
2023-09-04 19:10:53 +0200

    change way of loading pdf using pdfbox

--- src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/interfaces/PdfProductsExtractor.kt
+++ src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/interfaces/PdfProductsExtractor.kt
@@ -2,6 +2,8 @@ package com.stefanbratanov.sofiasupermarketsapi.interfaces
 
 import com.stefanbratanov.sofiasupermarketsapi.model.Product
 import java.nio.file.Path
+import org.apache.pdfbox.Loader
+import org.apache.pdfbox.io.RandomAccessReadBufferedFile
 import org.apache.pdfbox.pdmodel.PDDocument
 
 interface PdfProductsExtractor {
@@ -9,6 +11,6 @@ interface PdfProductsExtractor {
   fun extract(pdf: Path): List<Product>
 
   fun getPDocument(pdf: Path): PDDocument {
-    return PDDocument.load(pdf.toFile())
+    return Loader.loadPDF(RandomAccessReadBufferedFile(pdf.toFile()))
   }
 }
