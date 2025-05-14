c50e6276d13ba218a46576316a863432db73fad8
StefanBratanov <stefan.bratanov93@gmail.com>
2025-05-14 09:57:44 +0100

    Fix build

--- src/test/kotlin/com/stefanbratanov/sofiasupermarketsapi/brochure/FantasticoBrochureDownloaderTest.kt
+++ src/test/kotlin/com/stefanbratanov/sofiasupermarketsapi/brochure/FantasticoBrochureDownloaderTest.kt
@@ -5,6 +5,7 @@ import assertk.assertions.endsWith
 import assertk.assertions.exists
 import java.net.URL
 import java.nio.file.Files
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 internal class FantasticoBrochureDownloaderTest {
@@ -12,7 +13,7 @@ internal class FantasticoBrochureDownloaderTest {
   private lateinit var underTest: FantasticoBrochureDownloader
 
   @Test
-  @Disabled("need to change the flow")
+  @Disabled("need to change the downloading logic")
   fun `downloads real brochure`() {
     underTest = FantasticoBrochureDownloader(URL("https://www.fantastico.bg/special-offers"))
     val result = underTest.download()
