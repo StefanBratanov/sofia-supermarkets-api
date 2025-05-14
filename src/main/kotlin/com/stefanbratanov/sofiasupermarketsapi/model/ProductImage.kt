95a66c118805b8ed20a0d49dab3b0a1749a628e9
StefanBratanov <stefan.bratanov93@gmail.com>
2025-05-14 10:24:55 +0100

    Increase column length to satisfy H2

--- src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/model/ProductImage.kt
+++ src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/model/ProductImage.kt
@@ -1,6 +1,11 @@
 package com.stefanbratanov.sofiasupermarketsapi.model
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 
-@Entity data class ProductImage(@Id val product: String, var url: String?)
+@Entity
+data class ProductImage(
+  @Id @Column(columnDefinition = "TEXT") val product: String,
+  @Column(columnDefinition = "TEXT") var url: String?,
+)
