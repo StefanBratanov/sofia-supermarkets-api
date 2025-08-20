716387d9650f9292afacce4d78b533f35f1c7f01
StefanBratanov <stefan.bratanov93@gmail.com>
2025-08-20 15:43:57 +0100

    Try to set user-agent

--- src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/common/FileUitls.kt
+++ src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/common/FileUitls.kt
@@ -6,7 +6,13 @@ import java.nio.file.Path
 import java.nio.file.StandardCopyOption
 
 fun copyURLToFile(source: URL, destination: Path): Long {
-  return source.openStream().use { inputStream ->
+  val connection = source.openConnection()
+  // Set a Browser-like User-Agent
+  connection.setRequestProperty(
+    "User-Agent",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+  )
+  return connection.inputStream.use { inputStream ->
     Files.copy(inputStream, destination, StandardCopyOption.REPLACE_EXISTING)
   }
 }
