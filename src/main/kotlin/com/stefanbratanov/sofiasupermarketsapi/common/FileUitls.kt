cbb4e768c6091b7c5fcba6d3de0067799fd5d7b5
StefanBratanov <stefan.bratanov93@gmail.com>
2025-08-20 16:09:00 +0100

    Trying to use ApacheHttp client

--- src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/common/FileUitls.kt
+++ src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/common/FileUitls.kt
@@ -1,18 +1,29 @@
 package com.stefanbratanov.sofiasupermarketsapi.common
 
+import java.io.FileOutputStream
 import java.net.URL
-import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.StandardCopyOption
+import kotlin.io.path.fileSize
+import org.apache.hc.client5.http.classic.methods.HttpGet
+import org.apache.hc.client5.http.impl.classic.HttpClients
 
 fun copyURLToFile(source: URL, destination: Path): Long {
-  val connection = source.openConnection()
-  // Set a Browser-like User-Agent
-  connection.setRequestProperty(
-    "User-Agent",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-  )
-  return connection.inputStream.use { inputStream ->
-    Files.copy(inputStream, destination, StandardCopyOption.REPLACE_EXISTING)
+  val client = HttpClients.createDefault()
+  val request =
+    HttpGet(source.toURI()).apply {
+      setHeader(
+        "User-Agent",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+      )
+    }
+
+  client.execute(request) {
+    it.entity!!.content.use { inputStream ->
+      FileOutputStream(destination.toFile()).use { outputStream ->
+        inputStream.copyTo(outputStream)
+      }
+    }
   }
+
+  return destination.fileSize()
 }
