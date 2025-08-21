187e48fcfa9f5839484b4ec04ab123ea34069e5f
StefanBratanov <stefan.bratanov93@gmail.com>
2025-08-21 10:16:01 +0100

    Don't run flows if already running

--- src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/scheduled/ScheduledFlowsRunner.kt
+++ src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/scheduled/ScheduledFlowsRunner.kt
@@ -6,14 +6,22 @@ import com.stefanbratanov.sofiasupermarketsapi.interfaces.SupermarketFlow
 import com.stefanbratanov.sofiasupermarketsapi.model.Supermarket
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.util.concurrent.atomic.AtomicBoolean
 
 @Log
 @Component
 class ScheduledFlowsRunner(val flows: Map<Supermarket, SupermarketFlow>) {
 
+  private val isRunning = AtomicBoolean(false)
+
   @Scheduled(cron = "\${flows.runner.cron}")
   fun runFlows() {
+    if (!isRunning.compareAndSet(false, true)) {
+      log.warn("Already running. Skipping this execution.")
+      return
+    }
     log.info("Scheduled to run flows for {}", flows.keys)
     flows.forEach { (_, flow) -> flow.runSafely() }
+    isRunning.set(false)
   }
 }
