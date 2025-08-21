7d17ba696d23c7e4987dd77a566832f90bd9771e
StefanBratanov <stefan.bratanov93@gmail.com>
2025-08-21 10:18:49 +0100

    Fix spotless

--- src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/scheduled/ScheduledFlowsRunner.kt
+++ src/main/kotlin/com/stefanbratanov/sofiasupermarketsapi/scheduled/ScheduledFlowsRunner.kt
@@ -4,9 +4,9 @@ import com.stefanbratanov.sofiasupermarketsapi.common.Log
 import com.stefanbratanov.sofiasupermarketsapi.common.Log.Companion.log
 import com.stefanbratanov.sofiasupermarketsapi.interfaces.SupermarketFlow
 import com.stefanbratanov.sofiasupermarketsapi.model.Supermarket
+import java.util.concurrent.atomic.AtomicBoolean
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.util.concurrent.atomic.AtomicBoolean
 
 @Log
 @Component
